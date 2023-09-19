eee8339cab25d14375f1f8ba470b7feee22f4dff
nerd <nerd@AOI-no-PC>
2023-09-19 22:40:05 +0900

    2023-09-18 課題1.16 curriculumB Qes_1_3.4.5 修正

--- HelloWorld_OtaniToya/src/curriculum_B/Qes_4.java
+++ HelloWorld_OtaniToya/src/curriculum_B/Qes_4.java
@@ -15,16 +15,11 @@ public class Qes_4 {
 			for(int num2 = 1; num2 <= 9; num2++) {
 				
 				// 二桁表示にして表示
-				System.out.printf("%02d * %02d = %02d" ,num1,num2,(num1 * num2));
+				System.out.printf("%02d * %02d = %02d" ,num1 ,num2 ,(num1 * num2));
 				
 				// 列が9の時の判定
-				if(num2 != 9) {
-					//"||"を出力
-					System.out.print(" || ");
-				}else {
-					//改行
-					System.out.println();
-				}
+				//"||"or "\n"を出力
+				System.out.print(num2 != 9 ? "||" : "\n");
 			}
 		}
 	}
--- HelloWorld_OtaniToya/src/curriculum_B/Qes_5.java
+++ HelloWorld_OtaniToya/src/curriculum_B/Qes_5.java
@@ -5,26 +5,22 @@ public class Qes_5 {
 	 *[概要]三桁表示の九*二十
 	 *[詳細]九*二十の表を表示
 	 ********************************************/
-	
+
 	public static void main(String[] args) {
-		
+
 		// 行のループ
 		for(int num1 = 1; num1 <= 9; num1++) {
-			
+
 			// 列のループ
 			for(int num2 = 1; num2 <= 20; num2++) {
-				
+
 				// 三桁表示にして表示
-				System.out.printf("%03d * %03d = %03d" ,num1,num2,(num1 * num2));
-				
+				System.out.printf("%03d * %03d = %03d" ,num1 ,num2 ,(num1 * num2));
+
 				// 列が20の時の判定
-				if(num2 != 20) {
-					//"||"を出力
-					System.out.print(" || ");
-				}else {
-					//改行
-					System.out.println();
-				}
+				//"||"or "\n"を出力
+				System.out.print(num2 != 20 ? "||" : "\n");
+
 			}
 		}
 	}
